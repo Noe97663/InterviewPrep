5986ca7746b650ad966f4cd4ed8b2c6e5e70e18c
Noe97663 <73392168+Noe97663@users.noreply.github.com>
2025-02-05 14:27:47 +0530

    36/75 - day 6

---
+++ Leetcode/binaryTree(8)/kthSmallest.py
@@ -0,0 +1,46 @@
+# given a BST, find the k'th number
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+## Brute force - traverse all nodes, put into array,
+##               sort array - O(n logn), O(n)
+## In-order Traversal - In-order traversal, put into array,
+##                      return k'th element - O(n), O(n)
+## DFS using in-order concept - slightly better - O(n), O(n)
+
+
+# In-order traversal
+def kthSmallest(self, root: [TreeNode], k: int) -> int:
+    arr = []
+
+    def dfs(node):
+        if not node:
+            return
+        dfs(node.left)
+        arr.append(node.val)
+        dfs(node.right)
+
+    dfs(root)
+    return arr[k - 1]
+
+
+# Iterative DFS
+def kthSmallest(self, root: [TreeNode], k: int) -> int:
+    stack = []
+    curr = root
+    while stack or curr:
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+        curr = stack.pop()
+        k -= 1
+        if k == 0:
+            return curr.val
+        curr = curr.right
---
+++ Leetcode/bitManipulation(2)/missingNo.py
@@ -0,0 +1,23 @@
+# range [0-n], but only n-1 spots in array
+# find the missing number
+
+## Brute force - sort array, start for loop,
+##               if i!=Arr[i], - O(n logn), O(n)
+## Hash set - use set to hash all arr nums,
+##            for loop, if i not in set, BOOM
+##            - O(n), O(n)
+## Sum subtraction - sum of array minus sum of
+#                    range - O(n), O(1)
+## Bitwise XOR - XOR with the same number gives
+##              0, loop through array, i XOR A[i]
+##              final remaining number wont get
+##              cancelled out - O(n), O(1)
+
+
+# XOR solution
+def missingNumber(self, nums: [int]) -> int:
+    n = len(nums)
+    xor = n
+    for i in range(n):
+        xor ^= i ^ nums[i]
+    return xor
