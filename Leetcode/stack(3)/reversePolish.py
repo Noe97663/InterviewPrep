fd26f3970539ffebf1a07416d373479d5a866c99
NoelMP <noelpmartin@gmail.com>
2025-08-25 22:00:55 -0700

    94/150 - day 25

---
+++ Leetcode/stack(3)/reversePolish.py
@@ -0,0 +1,20 @@
+# evaluate reverse polish notation array
+
+# Stack - O(n), O(n)
+
+def evalRPN(self, tokens: List[str]) -> int:
+    stack = []
+    for c in tokens:
+        if c == "+":
+            stack.append(stack.pop() + stack.pop())
+        elif c == "-":
+            a, b = stack.pop(), stack.pop()
+            stack.append(b - a)
+        elif c == "*":
+            stack.append(stack.pop() * stack.pop())
+        elif c == "/":
+            a, b = stack.pop(), stack.pop()
+            stack.append(int(float(b) / a))
+        else:
+            stack.append(int(c))
+    return stack[0]
